177cbd2ba178d51e15973876cfefa1914a6b1fbe
Vladyslav Domariev <s19314@pjwstk.edu.pl>
2021-06-02 20:07:42 +0300

    Create locations Version 0.1

---
+++ mini_doom.pl
@@ -0,0 +1,53 @@
+/** 3.  Elementy dynamiczne  */
+:- dynamic whereami/1.
+
+/** 1.1 Fakty DOWN */
+room(complex_oak,'comlex OAK'). /** Или через dynamic или через list или как второй агрумент добавить информацию о вещах в комнате. */
+room(exit_to_mars, 'Exit to the surface of Mars{END_POINT}').
+room(sarcophagus, 'Room with doomguys sarcophagus').
+room(raspres_center, 'RaspRes Center').
+room(argent_tower, 'Argent Tower').
+room(argent_complex, 'Argent Complex').
+room(vega_technical_department, 'Vega Technical Department').
+/** 1.1 Fakty UP */
+
+
+/** 1.2. Termy złożone DOWN */
+transit(raspres_center, exit_to_mars).
+transit(raspres_center, complex_oak).
+transit(raspres_center, vega_technical_department).
+transit(raspres_center, argent_tower).
+
+transit(argent_tower, raspres_center).
+transit(argent_tower, vega_technical_department).
+transit(argent_tower, argent_complex).
+
+transit(vega_technical_department, raspres_center).
+transit(vega_technical_department, argent_complex).
+transit(vega_technical_department, argent_tower).
+transit(vega_technical_department, complex_oak).
+
+transit(complex_oak, raspres_center).
+transit(complex_oak, argent_complex).
+transit(complex_oak, vega_technical_department).
+
+transit(sarcophagus, complex_oak).
+/** 1.2. Termy złożone UP */
+
+
+whereami(sarcophagus).
+/** 2.  Klauzula DOWN */
+info :- whereami(X), room(X,Y), write('Room name: '), write(Y), nl, fail.
+/** 2.  Klauzula UP */
+
+
+/** 2.  Rekurencja DOWN */
+road(X,Y) :- write('Show possible ways:\t'), transit(X,Y) ; room(X,Y).
+road(X,Y) :- transit(X,Z), road(Z,Y).
+/** 2.  Rekurencja UP */
+
+
+ /** 3.  Elementy dynamiczne  add and remove */
+
+  /** 4.  Uniﬁkacja, cięcie */
+/** 5.  Listy (operacje na listach) */
